125c6fb6f69c6073496f0b0bebb78e69389c2721
zhuofanw <zhuofanw@sfu.ca>
2021-01-14 17:21:41 +0800

    react脚手架demo

---
+++ 14.create_react_app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Hello from "./components/Hello/index";
+import Welcome from "./components/Welcome/index";
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<Hello />
+				<Welcome />
+			</div>
+		);
+	}
+}
