04158c8fd66907e3b35b080fa648b40ca4e22295
zhuofanw <zhuofanw@sfu.ca>
2021-01-19 12:09:21 +0800

    标签体内容也可以通过props传递

---
+++ 20.封装NavLink/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Header from "./component/Header";
+import MyNavLink from "./component/MyNavLink";
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<div className="col-xs-offset-2 col-xs-8">
+						<Header />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-2 col-xs-offset-2">
+						<div className="list-group">
+							{/* 在react中靠路由连接实现切换组件--编写路由链接*/}
+							<MyNavLink to="/about">About</MyNavLink>
+							<MyNavLink to="/home">Home</MyNavLink>
+						</div>
+					</div>
+					<div className="col-xs-6">
+						<div className="panel">
+							<div className="panel-body">
+								<Route path="/about" component={About} />
+								<Route path="/home" component={Home} />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
