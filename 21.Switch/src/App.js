23bf0baad153a1c9b64278e377a04fba9b486e66
zhuofanw <zhuofanw@sfu.ca>
2021-01-19 13:03:29 +0800

    解决多层路由css丢失问题/Switch/exact的使用

---
+++ 21.Switch/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Header from "./component/Header";
+import MyNavLink from "./component/MyNavLink";
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<div className="col-xs-offset-2 col-xs-8">
+						<Header />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-2 col-xs-offset-2">
+						<div className="list-group">
+							{/* 在react中靠路由连接实现切换组件--编写路由链接*/}
+							<MyNavLink to="/about">About</MyNavLink>
+							<MyNavLink to="/home">Home</MyNavLink>
+						</div>
+					</div>
+					<div className="col-xs-6">
+						<div className="panel">
+							<div className="panel-body">
+								{/* 注册路由 */}
+								<Switch>
+									<Route exact path="/about" component={About} />
+									<Route exact path="/home" component={Home} />
+								</Switch>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
---
+++ 21.Switch/src/component/MyNavLink/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+export default class MyNavLink extends Component {
+	render() {
+		return (
+			<div>
+				<NavLink
+					activeClassName="linkCss"
+					className="list-group-item"
+					{...this.props}
+				/>
+			</div>
+		);
+	}
+}
